1960a6a44e7fc9e14c9b9e16e3583ae52fe599f3
kcenizaj <150229810+kcenizaj@users.noreply.github.com>
2023-11-12 18:06:00 +0800

    Update box.c
    converted for loop box to do...while loop

--- Practice/box.c
+++ Practice/box.c
@@ -20,18 +20,46 @@
 //* loop i = 4: *****
 
 int main(){
+  
   int n = 0;
   printf("Enter n: ");
   scanf("%d", &n);
   
+  //FOR LOOP CODE!!
   //* Loop para sa row
-  for(int i = 0; i < n; ++i){
+ /* for(int i = 0; i < n; ++i)
+      {
+        
+        //* loop para sa column
+        for(int j = 0; j < n; ++j){
+          printf(" * ");
+        }
+        
+        printf("\n");
+      }
+  */
+
+
+  //DO WHILE LOOP 
+  //row
+  int row = 0;
+  do 
+    {
+        printf("\n Loop %d: ",row + 1);
+      	++row;
+          
+    			    //column
+              int column = 0; //ooh so you need to reset comlumn back to zero for each row
+        			do
+        				{
+                  printf(" chico ");
+                  ++column;			
+        				}
+        			while(column < n);	
+      } 
     
-    //* loop para sa column
-    for(int j = 0; j < n; ++j){
-      printf(" * ");
-    }
-    
-    printf("\n");
-  }
+  while(row < n);
+
+  
+return 0;
 }
